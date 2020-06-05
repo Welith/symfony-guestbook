1d106cb9494a358ebc5053e04d03c20a21ec2e61
Welith <bkolev95@gmail.com>
2020-06-05 16:43:52 +0300

    Database settings WIP

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,23 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/hello/{name}", name="homepage")
      */
-    public function index()
+    public function index(String $name)
     {
 //        return $this->render('conference/index.html.twig', [
 //            'controller_name' => 'ConferenceController',
 //        ]);
+        $greet = "";
+        if ($name) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
         return new Response(<<< EOF
 <html>
     <body>
-    <img src="../../public/images/under.jpg"/>
+    $greet
+    <div style="text-align: center">
+    <img src="/images/under.jpg" />
+</div>
 </body>
 </html>
 EOF
